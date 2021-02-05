694c8d59a545674e56b5e547cb117a32db483ab9
LilasX <LilasX@users.noreply.github.com>
2021-02-05 13:59:35 -0500

    Character and Camera Setup Completed

--- Assets/LocomotionCharacterController.cs
+++ Assets/LocomotionCharacterController.cs
@@ -29,10 +29,23 @@ public class LocomotionCharacterController : MonoBehaviour
     private float inputHor, inputVer = 0f; //Horizontal and Vertical Shifting
     private bool inputJump = false; //Jump
 
+    //Animations
+    private Animator anim; //Reference of the animation system
+    private float animVelocity; //parameter for the intensity of walking/running
+
+    //Jumps animations
+    [SerializeField] private LayerMask everythingButPlayer; //every layers except for the player
+    [SerializeField] private float distanceFromGround = 0.25f; //Which distance is considered falling
+    private float capsuleScale; //character's height by default
+    private CapsuleCollider capsule; //Reference to the capsule collider
+
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>(); //Controller cache
+        anim = GetComponent<Animator>(); //Animator cache
+        capsuleScale = controller.height; //Capsule height by default cache
+        capsule = GetComponent<CapsuleCollider>(); //Capsule collider cache
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -47,6 +60,37 @@ public class LocomotionCharacterController : MonoBehaviour
         inputJump = context.performed;
     }
 
+    void ScaleCharacter(float fraction)
+    {
+        controller.height = capsuleScale * fraction; //Change for the % of the original height
+        controller.center = new Vector3(0, controller.height * 0.5f, 0);
+
+        capsule.height = capsuleScale * fraction; //Change for the % of the original height
+        capsule.center = new Vector3(0, capsule.height * 0.5f, 0);
+    }
+
+    bool CheckIfGrounded()
+    {
+        if (controller.isGrounded)
+        {
+            return true; //already on the ground
+        }
+
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position,Vector3.down,out hit, everythingButPlayer))
+        {
+            if (hit.distance < distanceFromGround)
+            {
+                return true; //ground is close
+            }
+            else
+            {
+                return false; //ground is far
+            }
+        }
+        return false; //no ground under player
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -54,12 +98,18 @@ public class LocomotionCharacterController : MonoBehaviour
         groundedPlayer = controller.isGrounded; //if the character is on the ground
         if (groundedPlayer)
         {
+            if (controller.height != capsuleScale) //grounded yet capsule height is reduced
+            {
+                ScaleCharacter(1f); //return the capsule to its original height
+            }
+
             playerVelocity = new Vector3(inputHor, 0, inputVer); //Moving left to right, up and down
 
             //Change direction
             angleOffset = camera.eulerAngles.y; //Save the degrees in Y axis of the camera
             moveAngle = (Mathf.Atan2(playerVelocity.x, playerVelocity.z) * Mathf.Rad2Deg) + angleOffset; //Movement angle
             lookAngle = Mathf.LerpAngle(transform.eulerAngles.y, moveAngle, turnSmooth); //Turn progressively toward desired angle
+            animVelocity = playerVelocity.magnitude;
 
             if (playerVelocity.magnitude >= 0.1f) //Change angle only if we're moving
             {
@@ -70,11 +120,23 @@ public class LocomotionCharacterController : MonoBehaviour
             
             playerVelocity *= speed; //Increase velocity of the character
 
+            //Jump
             if (inputJump)
             {
                 inputJump = false;
+                anim.SetTrigger("Jump"); //play jump animation
                 playerVelocity.y = jumpIntensity; //Force up
+                ScaleCharacter(0.75f); //in the air, the character height is 75% its original height
             }
+        }
+
+        //Animations
+        anim.SetFloat("Magnitude", animVelocity); //Speed control of the animation walking and running
+        anim.SetBool("Grounded", CheckIfGrounded());
+        anim.SetFloat("Y", playerVelocity.y);
+        if (playerVelocity.y < 0 && controller.height == capsuleScale && !CheckIfGrounded())
+        {
+            ScaleCharacter(0.75f); //in the air, the character height is 75% its original height
         }
 
         //Apply gravity
@@ -89,5 +151,3 @@ public class LocomotionCharacterController : MonoBehaviour
 // 1- Copied from the Locomotion_CharacterController script from class project Integration de personnage
 // https://youtu.be/AmBlouhDvr8
 // https://youtu.be/3EAS9OkvJIk
-
-//current :https://youtu.be/3EAS9OkvJIk?t=440
